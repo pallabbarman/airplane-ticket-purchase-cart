2aa2efdf388b1a749d212ee086ec48f987deca92
Pallab Barman <76774117+pallabbarman@users.noreply.github.com>
2021-01-30 13:01:09 +0600

    cart part finished

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-function purchasingTicket(isIncrease) {
-    const ticketInput = document.getElementById('ticket-count');
+// purchases ticket 
+function purchasingTicket(ticketClass, isIncrease) {
+    const ticketInput = document.getElementById(ticketClass + '-ticket-count');
     const ticketCount = parseInt(ticketInput.value);
     let ticketNewCount = ticketCount;
     if (isIncrease == true) {
@@ -9,4 +10,31 @@ function purchasingTicket(isIncrease) {
         ticketNewCount = ticketCount - 1;
     }
     ticketInput.value = ticketNewCount;
+    let ticketTotal = 0;
+    if (ticketClass == 'fistClass') {
+        ticketTotal = ticketNewCount * 150;
+    }
+    if (ticketClass == 'economy') {
+        ticketTotal = ticketNewCount * 100;
+    }
+    calculateTotalAmount();
+}
+
+// total amount calculate 
+function calculateTotalAmount() {
+    const firstClassQuantity = getTicketClassValue('firstClass');
+    const economyQuantity = getTicketClassValue('economy');
+    const totalPrice = firstClassQuantity * 150 + economyQuantity * 100;
+    document.getElementById('total-price').innerText = totalPrice;
+    const tax = Math.round(totalPrice * 0.1);
+    document.getElementById('tax-amount').innerText = tax;
+    const totalAmount = totalPrice + tax;
+    document.getElementById('total-amount').innerText = totalAmount;
+}
+
+// ticket class value 
+function getTicketClassValue(ticketClass) {
+    const ticketInput = document.getElementById(ticketClass + '-ticket-count');
+    const ticketCount = parseInt(ticketInput.value);
+    return ticketCount;
 }
