ec16fed7adfebd306381286817b201a37e93bdf4
Pallab Barman <76774117+pallabbarman@users.noreply.github.com>
2021-01-30 21:21:13 +0600

    finished project

--- js/script.js
+++ js/script.js
@@ -26,10 +26,13 @@ function calculateTotalAmount() {
     const economyQuantity = getTicketClassValue('economy');
     const totalPrice = firstClassQuantity * 150 + economyQuantity * 100;
     document.getElementById('total-price').innerText = totalPrice;
+    document.getElementById("totalPrice").innerText = totalPrice;
     const tax = Math.round(totalPrice * 0.1);
     document.getElementById('tax-amount').innerText = tax;
+    document.getElementById('taxAmount').innerText = tax;
     const totalAmount = totalPrice + tax;
     document.getElementById('total-amount').innerText = totalAmount;
+    document.getElementById('totalAmount').innerText = totalAmount;
 }
 
 // ticket class value 
@@ -41,9 +44,18 @@ function getTicketClassValue(ticketClass) {
 
 // book now event handler
 document.getElementById("book-now").addEventListener('click', function () {
-    const purchasesArea = document.getElementById("purchase-area");
-    purchasesArea.style.display = "none";
-    const receiptArea = document.getElementById("receipt-area");
-    receiptArea.style.display = "block";
-
+    const firstClassQuantity = document.getElementById("firstClass-ticket-count").value;
+    const economyQuantity = document.getElementById("economy-ticket-count").value;
+    if (firstClassQuantity > 0 || economyQuantity > 0) {
+        const purchasesArea = document.getElementById("purchase-area");
+        purchasesArea.style.display = "none";
+        const receiptArea = document.getElementById("receipt-area");
+        receiptArea.style.display = "block";
+        document.getElementById("firstClass").innerText = firstClassQuantity;
+        document.getElementById("economyClass").innerText = economyQuantity;
+        calculateTotalAmount();
+    }
+    else {
+        alert("Please buy tickets first");
+    }
 })
